8bd8dc6cb418cc34c06cd8d611094395daaf82e2
John Hughes <jhughes314@users.noreply.github.com>
2020-05-10 12:46:29 -0400

    Update kmeans R John.R

--- Section 24 - K-Means Clustering/kmeans R John.R
+++ Section 24 - K-Means Clustering/kmeans R John.R
@@ -1,15 +1,40 @@
-# Data Preprocessing Template
+# K-means Clustering
 
 # Importing the dataset
-dataset = read.csv('Data.csv')
+dataset = read.csv('Mall_Customers.csv')
+dataset = dataset[4:5]
+
+# Using the Elbow Method to determine number of clusters
+set.seed(6)
+wcss = vector()
+for (i in 1:10) wcss[i] = sum(kmeans(dataset, i)$withinss)
+plot(1:10, wcss, type = "b", main = paste('Clusters of clients'), xlab = "Number of clusters", ylab = "WCSS")
+
+#Applying k-means to the dataset
+set.seed(29)
+kmeans = kmeans(X, 5, iter.max = 300, nstart = 10)
+
+#Visualizing the clusters
+library(cluster)
+clusplot(X,
+         kmeans$cluster,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('cluster of clients'),
+         xlab = 'annual income',
+         ylab = 'spending score')
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
 # training_set = scale(training_set)
