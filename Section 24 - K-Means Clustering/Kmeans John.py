5759e303005aef8ed3bdb604ddba831289ec28e9
John Hughes <jhughes314@users.noreply.github.com>
2020-05-10 11:25:31 -0400

    Scatter Plot

--- Section 24 - K-Means Clustering/Kmeans John.py
+++ Section 24 - K-Means Clustering/Kmeans John.py
@@ -35,7 +35,20 @@ plt.xlabel('number of clusters')
 plt.ylabel('WCSS')
 plt.show()
 
+#predict the clusters
 kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 42)
 y_kmeans = kmeans.fit_predict(X)
-
 print(y_kmeans)
+
+#Create Scatter Plot of Clusters
+plt.scatter(X[y_kmeans == 0,0], X[y_kmeans == 0,1], s=100, c = 'red', label = 'cluster 1')
+plt.scatter(X[y_kmeans == 1,0], X[y_kmeans == 1,1], s=100, c = 'blue', label = 'cluster 2')
+plt.scatter(X[y_kmeans == 2,0], X[y_kmeans == 2,1], s=100, c = 'green', label = 'cluster 3')
+plt.scatter(X[y_kmeans == 3,0], X[y_kmeans == 3,1], s=100, c = 'cyan', label = 'cluster 4')
+plt.scatter(X[y_kmeans == 4,0], X[y_kmeans == 4,1], s=100, c = 'yellow', label = 'cluster 5')
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], s=300, c = 'orange', label = 'centroids')
+plt.title('cluster of customers')
+plt.legend()
+plt.xlabel('annual income $')
+plt.ylabel('spending score')
+plt.show()
