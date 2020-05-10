eb916dbc7dbf4f8665e8a69f1abd2f1d1b8f78a8
John Hughes <jhughes314@users.noreply.github.com>
2020-05-10 10:56:48 -0400

    Initial commit

---
+++ Section 24 - K-Means Clustering/Kmeans John.py
@@ -0,0 +1,37 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+"""y = dataset.iloc[:, -1].values"""
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
+
+# Use the Elbow method to choose the best number of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('the elbow method')
+plt.xlabel('number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
